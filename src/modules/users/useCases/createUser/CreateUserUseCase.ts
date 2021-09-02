64ddaa2c52a9bb3ddd4f93af57a47ff1d16816f5
WendelSantosD <wendelwcsantos@gmail.com>
2021-09-02 10:32:32 -0300

    CreateUseCase.spec.ts

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,17 +1,25 @@
-import { User } from "../../model/User";
-import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { User } from '../../model/User'
+import { IUsersRepository } from '../../repositories/IUsersRepository'
 
 interface IRequest {
-  name: string;
-  email: string;
+    name: string
+    email: string
 }
 
 class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ email, name }: IRequest): User {
-    // Complete aqui
-  }
+    execute({ email, name }: IRequest): User {
+        const userAlreadyExists = this.usersRepository.findByEmail(email)
+
+        if (userAlreadyExists) {
+            throw new Error('ser already exists')
+        }
+
+        const user = this.usersRepository.create({ name, email })
+
+        return user
+    }
 }
 
-export { CreateUserUseCase };
+export { CreateUserUseCase }
