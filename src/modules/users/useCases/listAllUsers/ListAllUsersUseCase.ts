c21bc2899407c77664ea667464a00f4047a4f037
WendelSantosD <wendelwcsantos@gmail.com>
2021-09-02 11:45:40 -0300

    ListAllUsersUseCase.spec.ts

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,16 +1,23 @@
-import { User } from "../../model/User";
-import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { User } from '../../model/User'
+import { IUsersRepository } from '../../repositories/IUsersRepository'
 
 interface IRequest {
-  user_id: string;
+    user_id: string
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): User[] {
+        const adminUser = this.usersRepository.findById(user_id)
+        const users = this.usersRepository.list()
+
+        if (adminUser.admin) {
+            return users
+        } else {
+            throw new Error('no users')
+        }
+    }
 }
 
-export { ListAllUsersUseCase };
+export { ListAllUsersUseCase }
