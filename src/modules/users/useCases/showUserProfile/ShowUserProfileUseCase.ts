fb7a77ec91418eaa205de68237541cada97f1435
WendelSantosD <wendelwcsantos@gmail.com>
2021-09-02 11:38:32 -0300

    ShowUserProfileUseCase.spec.ts

--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -1,16 +1,22 @@
-import { User } from "../../model/User";
-import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { User } from '../../model/User'
+import { IUsersRepository } from '../../repositories/IUsersRepository'
 
 interface IRequest {
-  user_id: string;
+    user_id: string
 }
 
 class ShowUserProfileUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): User {
+        const user = this.usersRepository.findById(user_id)
+
+        if (user) {
+            return user
+        } else {
+            throw new Error('user not found')
+        }
+    }
 }
 
-export { ShowUserProfileUseCase };
+export { ShowUserProfileUseCase }
