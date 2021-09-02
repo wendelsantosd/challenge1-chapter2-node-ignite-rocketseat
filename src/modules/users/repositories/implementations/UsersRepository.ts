f8441cd1f6f19df056b0f67d3f88474451a99248
WendelSantosD <wendelwcsantos@gmail.com>
2021-09-02 10:04:16 -0300

    UsersRepository.spec.ts

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,42 +1,68 @@
-import { User } from "../../model/User";
-import { IUsersRepository, ICreateUserDTO } from "../IUsersRepository";
+import { User } from '../../model/User'
+import { IUsersRepository, ICreateUserDTO } from '../IUsersRepository'
 
 class UsersRepository implements IUsersRepository {
-  private users: User[];
+    private users: User[]
 
-  private static INSTANCE: UsersRepository;
+    private static INSTANCE: UsersRepository
 
-  private constructor() {
-    this.users = [];
-  }
-
-  public static getInstance(): UsersRepository {
-    if (!UsersRepository.INSTANCE) {
-      UsersRepository.INSTANCE = new UsersRepository();
+    private constructor() {
+        this.users = []
     }
 
-    return UsersRepository.INSTANCE;
-  }
+    public static getInstance(): UsersRepository {
+        if (!UsersRepository.INSTANCE) {
+            UsersRepository.INSTANCE = new UsersRepository()
+        }
 
-  create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
-  }
+        return UsersRepository.INSTANCE
+    }
 
-  findById(id: string): User | undefined {
-    // Complete aqui
-  }
+    create({ name, email }: ICreateUserDTO): User {
+        const user = new User()
 
-  findByEmail(email: string): User | undefined {
-    // Complete aqui
-  }
+        Object.assign(user, {
+            name,
+            email,
+            created_at: new Date(),
+            updated_at: new Date(),
+        })
 
-  turnAdmin(receivedUser: User): User {
-    // Complete aqui
-  }
+        this.users.push(user)
 
-  list(): User[] {
-    // Complete aqui
-  }
+        return user
+    }
+
+    findById(id: string): User | undefined {
+        const user = this.users.find(user => user.id === id)
+
+        if (user) {
+            return user
+        }
+    }
+
+    findByEmail(email: string): User | undefined {
+        const user = this.users.find(user => user.email === email)
+
+        if (user) {
+            return user
+        }
+    }
+
+    turnAdmin(receivedUser: User): User {
+        const userUpdate = { ...receivedUser }
+
+        Object.assign(userUpdate, {
+            admin: true,
+            updated_at: new Date(),
+        })
+
+        return userUpdate
+    }
+
+    list(): User[] {
+        return this.users
+    }
 }
 
-export { UsersRepository };
+export { UsersRepository }
